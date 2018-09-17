11b8c06e720489ec1ef5d556ac691ed3b161aee1
Gustavo Lopes <gustavo.ldbrito@gmail.com>
2018-09-17 14:57:28 -0300

    Update char by char to get words

--- charbychar.py
+++ charbychar.py
@@ -1,13 +1,15 @@
 from tesserocr import PyTessBaseAPI, RIL, iterate_level
+import sys
 
 with PyTessBaseAPI(lang='por') as api:
-    api.SetImageFile(input())
+    api.SetImageFile(sys.argv[1])
     api.SetVariable("save_blob_choices", "T")
     api.Recognize()
-
+    
+    print(' '.join(word for word in api.AllWords()))
     ri = api.GetIterator()
     level = RIL.SYMBOL
     for r in iterate_level(ri, level):
         symbol = r.GetUTF8Text(level)  # r == ri
         conf = r.Confidence(level)
-        print(symbol, end='')
+    print(api.GetUTF8Text())
